65e36e7961081902f9a2631e51dfd253208ead86
HarshadBhosale <harshad.bhosale@cogoport.com>
2022-10-25 13:35:24 +0530

    basic setup

---
+++ src/finance_tracker_app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FinanceTrackerApp = () => {
+    return(
+        <>
+            <h1>King Here</h1>
+        </>
+    );
+}
+
+export default FinanceTrackerApp;
---
+++ src/pages/user_registration.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserRegistration = () => {
+    return(
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+}
+
+export default UserRegistration;

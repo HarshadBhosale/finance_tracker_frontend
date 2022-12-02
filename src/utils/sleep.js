e0051d5a43eea0ad776b4bc983a9c0080dc051ab
HarshadBhosale <harshad.bhosale@cogoport.com>
2022-12-02 15:36:37 +0530

    sleep function added

---
+++ src/utils/sleep.js
@@ -0,0 +1,9 @@
+const Sleep = (milliseconds) => {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+export default Sleep;

6048f6ce664ded1f0aae97bcc391d3e3b95d28c1
HarshadBhosale <harshad.bhosale@cogoport.com>
2023-02-15 19:20:20 +0530

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import FinanceTrackerApp from "./finance_tracker_app";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
     <React.StrictMode>
         <FinanceTrackerApp />
